0460d3b2acf801cbf49f32d834db75029604346d
Patryk Baraniak <pb311239@student.polsl.pl>
2024-03-01 17:37:36 +0100

    update cos

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,42 @@
 const weatherForm = document.querySelector(".weatherForm") as HTMLFormElement;
 const cityInput = document.querySelector(".cityInput") as HTMLInputElement;
-const card = document.querySelector("card") as HTMLDivElement;
+const card = document.querySelector(".card") as HTMLDivElement;
 
 const apiKey = "9e01c78d7e1475f408562578ca1f257b";
 
-weatherForm.addEventListener("submit", (event) => {
+weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const city = cityInput.value;
 
   if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(String(error));
+    }
   } else {
     displayError("Please enter a city");
   }
 });
 
-async function getWeatherData(city: string) {}
+async function getWeatherData(city: string) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-function displayWeatherInfo(data: string) {}
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error("Could not fetch weather data");
+  } else {
+    return await response.json();
+  }
+}
+
+function displayWeatherInfo(data: string) {
+  console.log(data);
+}
 
 function getWeatherEmoji(weatherId: number) {}
 
